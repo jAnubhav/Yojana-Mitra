665628e615ecacdd5f2d4a51d2a6c972443c89f1
Anubhav Jain <jainanubhav2003@gmail.com>
2025-01-04 11:40:05 +0530

    fixed

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ app = Flask("YojanaMitra")
 
 @app.route("/")
 def home():
-    return render_template("dashboard/dashboard.html")
+    return render_template("dashboard/dashboard.html", active=0)
 
 @app.route("/schemes")
 def schemes():
-    return render_template("dashboard/schemes.html")
+    return render_template("dashboard/schemes.html", active=1)
 
 @app.route("/schemes/support_for_value_addition")
 def scheme():
-    return render_template("schemes/scheme.html")
+    return render_template("schemes/scheme.html", active=1)
 
 if __name__ == "__main__":
     app.run(debug=True)
